1e88abc37c4972a034cfbb1184beffe5ed45fa16
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-23 18:59:55 +0300

    change fields values

--- src/main/java/ru/philimonov/spring/mvc/MyController.java
+++ src/main/java/ru/philimonov/spring/mvc/MyController.java
@@ -15,16 +15,24 @@ public class MyController {
 
     @RequestMapping("/askDetails")
     public String askEmployeeDetails(Model model) {
-        Employee emp = new Employee();
-        emp.setName("Ivan");
-        emp.setSurname("Sidorov");
-        emp.setSalary(750);
-        model.addAttribute("employee", emp);
+//        Employee emp = new Employee();
+//        emp.setName("Ivan");
+//        emp.setSurname("Sidorov");
+//        emp.setSalary(750);
+        model.addAttribute("employee", new Employee());
         return "ask-emp-details-view";
     }
 
     @RequestMapping("/showDetails")
     public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+        String name = emp.getName();
+        emp.setName("Mr. " + name);
+
+        String surname = emp.getSurname();
+        emp.setSurname(surname + "!");
+
+        int salary = emp.getSalary();
+        emp.setSalary(salary * 20);
 
         return "show-emp-details-view";
     }
